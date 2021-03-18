3cda6fce0c5268a3884a3a3f9fa9e22da25ca05a
s0npaRi11 <paragmahale36@gmail.com>
2021-03-18 11:08:55 +0530

    fetching repos with github api

--- src/components/partials/ProjectCard.js
+++ src/components/partials/ProjectCard.js
@@ -6,7 +6,8 @@ const ProjectCard = ({ r }) => {
             <div className="project-card">
                 <h3 className="project-card-heading"> { r.name } </h3>
                 <p className="project-card-details"> { r.description }  |  { r.language }</p>
-                <div className="project-card-links"> <a href="{ r.html_url }"> <FaGithub/> </a>  <a href="#"> <FaGlobe/> </a></div>
+                <div className="project-card-links"> <a href="{ r.html_url }"> <FaGithub/> </a> 
+                </div>
             </div>
         </>
     )
