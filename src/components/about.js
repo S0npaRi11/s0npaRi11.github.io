5bbaa914f990236cae8de791084771394d47243d
s0npaRi11 <paragmahale36@gmail.com>
2021-04-02 09:00:01 +0530

    about me added

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,6 @@
 import Hamburger from './partials/Hamburger'
 import Contact from './partials/Contacts'
+import image from './parag-mahale.jpg';
 
 const about = () => {
     return (
@@ -11,7 +12,20 @@ const about = () => {
 
             <div className="content">
                 <div className="content-wrapper">
-                    Some Info About Me
+                   <div className="about-me-grid">
+                        <div className="about-me-info">
+                            <p>
+                                Hi, I am Parag Mahale (s0npaRi11 on github), a student, web designer and developer.
+                                I am currently studying in a Computer Engineering couese at <a href="#"> SSVPS's B.S.Deore College of Engineering </a> located at Dhule, Maharashtra, India. 
+                            </p>
+                            <p>
+                                I have worked on plenty of small scale projects and also have been working as a freelancer locally since past year.
+                            </p>
+                        </div>
+                        <div className="about-me-picture">
+                            <img src={image} alt="parag-mahale" />
+                        </div>
+                   </div>
                 </div>
             </div>
 
