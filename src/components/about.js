aca180997d6ee742d1b07069ea2ce0887988f83a
s0npaRi11 <paragmahale36@gmail.com>
2021-04-02 10:44:13 +0530

    links added, favicon added

--- src/components/about.js
+++ src/components/about.js
@@ -16,14 +16,14 @@ const about = () => {
                         <div className="about-me-info">
                             <p>
                                 Hi, I am Parag Mahale (s0npaRi11 on github), a student, web designer and developer.
-                                I am currently studying in a Computer Engineering couese at <a href="#"> SSVPS's B.S.Deore College of Engineering </a> located at Dhule, Maharashtra, India. 
+                                I am currently studying in a Computer Engineering couese at <a href="#"> SSVPS's B.S.Deore College of Engineering </a> located in Dhule, Maharashtra, India. 
                             </p>
                             <p>
                                 I have worked on plenty of small scale projects and also have been working as a freelancer locally since past year.
                             </p>
                         </div>
                         <div className="about-me-picture">
-                            <img src={image} alt="parag-mahale" />
+                            <img src={image} alt="parag-mahale" className="image"/>
                         </div>
                    </div>
                 </div>
--- src/components/partials/Contacts.js
+++ src/components/partials/Contacts.js
@@ -10,16 +10,16 @@ const Contacts = () => {
                        
                     </h1>
                     <div className="contacts font-regular"> 
-                        <a href="mailto:paragmahale36@gmail.com"> <FaAt /> : contact@email.com <br /> </a>
-                        <div>
-                            <a href="https://www.github.com" ><FaGithub /> </a>
+                        <a href="mailto:paragmahale36@gmail.com"> <FaAt /> </a>
+                        {/* <div> */}
+                            <a href="https://www.github.com/s0npaRi11" ><FaGithub /> </a>
                             
-                            <a href="https://www.linkedin.com">  <FaLinkedin />  </a>
-                            <a href="https://www.instagram.com"> <FaInstagram /> </a>
-                            <a href="https://www.facebook.com">  <FaFacebook /> </a>
+                            <a href="https://www.linkedin.com/in/parag-mahale-12b90019a/">  <FaLinkedin />  </a>
+                            <a href="https://www.instagram.com/parag450/"> <FaInstagram /> </a>
+                            <a href="https://www.facebook.com/parag.mahale/">  <FaFacebook /> </a>
 
                             {/* <div className="watermark"> Designed By : Parag Mahale </div> */}
-                        </div>
+                        {/* </div> */}
                     </div>
                 </div>
             </div>
