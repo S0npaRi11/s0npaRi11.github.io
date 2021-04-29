81c75de59ce00428bc6930d8fbcfc65e80ddbd85
s0npaRi11 <paragmahale36@gmail.com>
2021-04-29 09:43:20 +0530

    Updates

---
+++ actions.js
@@ -0,0 +1,22 @@
+const throttle = (fn, wait) => {
+    let time = Date.now();
+    return function () {
+        if(time + wait - Date.now() < 0){
+            fn();
+            time = Date.now();
+        }
+    }
+}
+
+const parallax = () => {
+    const scrolled = window.pageYOffset;
+    const parallax = document.querySelector('.parallax')
+    
+   if(parallax){
+    const coords = ( scrolled * 0.7 ) + 'px'
+    
+    parallax.style.transform = `translateY(${coords})`
+   }
+}
+
+ window.addEventListener('scroll',throttle(parallax, 14))
